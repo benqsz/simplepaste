d50dcffaf763c827b08e6960d4e4235db956019e
benqsz <beniamink123@gmail.com>
2026-06-02 00:16:15 +0200

    feat: code highlight

--- src/components/ui/md.tsx
+++ src/components/ui/md.tsx
@@ -1,4 +1,6 @@
-import { marked } from 'marked'
+import hljs from 'highlight.js'
+import { Marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
 
 import { cn } from '@/lib/utils'
 
@@ -6,6 +8,17 @@ type Props = {
   content: string
   className?: string
 }
+
+const marked = new Marked(
+  markedHighlight({
+    emptyLangClass: 'hljs',
+    langPrefix: 'hljs language-',
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext'
+      return hljs.highlight(code, { language }).value
+    },
+  }),
+)
 
 export function Md({ content, className }: Props) {
   return (
