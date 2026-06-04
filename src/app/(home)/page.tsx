6b2f86a0dfe73d5e704b5d46a898288e29ca34c3
benqsz <beniamink123@gmail.com>
2026-06-04 17:44:10 +0200

    chore: bump packages

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,7 +2,6 @@
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
-import { useEffect } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { z } from 'zod'
